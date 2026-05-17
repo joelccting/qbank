37489d13c487291b983a4bdaa67a509bf24be35c
Joel Ting <joelccting@gmail.com>
2026-05-17 13:02:20 +0800

    modified:   ac_458c/main.cpp

--- ac_458c/main.cpp
+++ ac_458c/main.cpp
@@ -2,19 +2,19 @@
 #include <cstring>
 
 using namespace std;
-
+typedef long long LL;
 char S[500005];
 
-int sol(int len)
+LL sol(int len)
 {
-    int ret = 0;
+    LL ret = 0;
     for (int i = 0; i < len; ++i)
     {
         if (S[i] != 'C') continue;
 
         int left = i;
         int right = len - i - 1;
-        ret += (left < right ? left : right);
+        ret += (left <= right ? left : right);
         ret += 1;
     }
 
